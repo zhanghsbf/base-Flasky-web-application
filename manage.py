f3c7015684effb9074e9a9e241e23918991df38c
zhangYK <1019437875@qq.com>
2018-03-07 18:08:10 +0800

    flask web

---
+++ manage.py
@@ -0,0 +1,68 @@
+import os
+from app import create_app, db
+from app.models import *
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role,Permission=Permission,Post=Post)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+	import coverage
+	COV = coverage.coverage(branch=True, include='app/*')
+	COV.start()
+
+@manager.command
+def test(coverage=False):
+	'''Run the unit tests.'''
+	if coverage and not os.environ.get('FLASK_COVERAGE'):
+		import sys
+		os.environ['FLASK_COVERAGE'] = '1'
+		os.execvp(sys.executable, [sys.executable] + sys.argv)
+	import unittest
+	tests = unittest.TestLoader().discover('tests')
+	unittest.TextTestRunner(verbosity=2).run(tests)
+	if COV:
+		COV.stop()
+		COV.save()
+		print('Coverage Summary:')
+		COV.report()
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		covdir = os.path.join(basedir, 'tmp/coverage')
+		COV.html_report(directory=covdir)
+		print('HTML version: file://%s/index.html' % covdir)
+		COV.erase()
+
+@manager.command
+def profile(length=25, profile_dir=None):
+	"""在代码监视器下运行程序"""
+	from werkzeug.contrib.profiler import ProfilerMiddleware
+	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+								profile_dir=profile_dir)
+	app.run()
+
+@manager.command
+def deploy():
+	"""Run deployment tasks."""
+	from flask_migrate import upgrade
+	from app.models import Role, User
+
+	# 数据库迁移
+	upgrade()
+
+	# 创建用户角色
+	Role.insert_roles()
+
+	# 所有用户关注自己（实现朋友圈看自己文章功能）
+	User.add_self_follows()
+
+if __name__ == '__main__':
+	manager.run()
